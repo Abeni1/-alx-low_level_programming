6133b359809168b3fdcfae705da7337661853c20
Abeni1 <95745749+Abeni1@users.noreply.github.com>
2022-04-07 19:04:58 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main()
+
+int main(void)
 {
   puts("\"Programming is like building a multilingual puzzel");
-  return(0);
+  return (0);
 }
