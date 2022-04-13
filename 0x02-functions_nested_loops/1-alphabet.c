74e5ecbf8350ebff078518acddece74a9b0cbf4b
Abeni1 <95745749+Abeni1@users.noreply.github.com>
2022-04-12 18:23:15 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,19 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
