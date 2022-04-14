9978ec5b9cd9f685662fa5626a0f96a08d5993b3
Abeni1 <samuelabenezer070@gmail.com>
2022-04-14 12:46:13 -0700

    pushing debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main - Test function for positive or negative
+ *Return: Always success (0)
+ */
+
+int main()
+{
+  int i;
+  i=0;
+  positive_or_negative(i);
+  return(0);
+}
